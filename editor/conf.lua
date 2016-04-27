2d299cdc2917f041743e45e2e4f091031da2f741
Wesley <wesley.werner@gmail.com>
2016-04-27 17:38:40 +0200

    Update editor config version

--- editor/conf.lua
+++ editor/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = "nova-pinball"
-    t.version = "0.9.1"
+    t.version = "0.10.1"
     t.console = false
     t.window.title = "Nova Pinball Table Editor"
     t.window.width = 1000
